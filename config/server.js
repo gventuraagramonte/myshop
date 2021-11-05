972c99653330e55b77f92dff21b0d44564e89cb2
Giorgio Ventura <gventuraagramonte@gmail.com>
2021-11-05 03:56:43 -0500

    first commit Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e3496c145a8ea0b04f955223e3b9ad1f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2b7f75cf-23f5-4f10-9d23-3d8c93d283be'
+};
